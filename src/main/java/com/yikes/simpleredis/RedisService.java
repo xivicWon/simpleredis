e8b51cb22ae3ea514e1f83ec8da22b9ed74ce538
xivic <xivic@kakao.com>
2025-06-04 23:26:02 +0900

    basic redis work
    # Conflicts:
    #	src/main/java/com/yikes/simpleredis/RedisService.java

--- src/main/java/com/yikes/simpleredis/RedisService.java
+++ src/main/java/com/yikes/simpleredis/RedisService.java
@@ -1,16 +1,14 @@
 package com.yikes.simpleredis;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RedisService {
 
     private final RedisTemplate<String, String> redisTemplate;
-
-    public RedisService(RedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     public void setValue(String key, String value) {
         redisTemplate.opsForValue().set(key, value);
